93100c526eaf18e6c902bfd8eea92045dee1cb8a
ivelco <ivelin_conchev@abv.bg>
2022-03-06 19:28:58 +0200

    Check if date before another date attribute added

---
+++ Football.Core/CustomAttribute/IsBeforeAttribute.cs
@@ -0,0 +1,33 @@
+﻿namespace Football.Core.CustomAttribute
+{
+    using System.ComponentModel.DataAnnotations;
+    public class IsBeforeAttribute : ValidationAttribute
+    {
+        private readonly string propertyCompare;
+        public IsBeforeAttribute(string _propertyCompare, string errorMessage = "")
+        {
+            propertyCompare = _propertyCompare;
+            this.ErrorMessage = errorMessage;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value == null) return null;
+
+            try
+            {
+                DateTime dateToCompare = (DateTime)validationContext
+               .ObjectType.GetProperty(propertyCompare).GetValue(validationContext.ObjectInstance);
+
+                if ((DateTime)value < dateToCompare)
+                {
+                    return ValidationResult.Success;
+                }
+            }
+            catch (Exception)
+            {
+            }
+            return new ValidationResult(ErrorMessage);
+        }
+    }
+}
